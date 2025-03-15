0699692c659767b869591b69266bf17fb9f0d534
Andre Migotto <andremigotto@icloud.com>
2025-03-15 19:08:37 -0300

    feat: Adding /users POST and GET structure to node server in memory

--- src/server.js
+++ src/server.js
@@ -1,6 +1,25 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
+    const { method, url } = req
+
+    if(method === 'GET' && url === '/users') {
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if(method === 'POST' && url === '/users') {
+        users.push({
+            id: '1',
+            name: 'Andre Migotto',
+            email: 'andremigotto@email.com' 
+        })
+        return res.end("Criacao de usuario")
+    }
+
     return res.end("Hello World")
 })
 
