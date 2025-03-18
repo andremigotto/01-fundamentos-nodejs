5bc708f86bba91710e5f450f6af4c376844a8d20
Andre Migotto <andremigotto@icloud.com>
2025-03-18 14:31:21 -0300

    feat: Adding req by a JSON body, removing hardcode example for POST method and GET method

--- src/server.js
+++ src/server.js
@@ -2,8 +2,20 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const { method, url } = req
+
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        req.body = null
+    }
 
     if(method === 'GET' && url === '/users') {
         return res
@@ -12,12 +24,15 @@ const server = http.createServer((req, res) => {
     }
 
     if(method === 'POST' && url === '/users') {
+
+        const {name, email} = req.body
+
         users.push({
-            id: '1',
-            name: 'Andre Migotto',
-            email: 'andremigotto@email.com' 
+            id: 1,
+            name,
+            email
         })
-        return res.writeHead(201).end()
+        return res.writeHead(201).end() 
     }
 
     return res.writeHead(404).end('Not Found')
