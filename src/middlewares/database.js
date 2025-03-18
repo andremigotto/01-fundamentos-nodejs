ffb5e2b24563a898574b16fbc24092c66667b422
Andre Migotto <andremigotto@icloud.com>
2025-03-18 15:12:49 -0300

    feat: Adding persist and constructor to database file in root on project

--- src/middlewares/database.js
+++ src/middlewares/database.js
@@ -1,7 +1,26 @@
-// { "users": [...]}
+import fs from 'node:fs/promises'
+
+const databasePath = new URL(
+    '../../db.json',
+    import.meta.url
+)
 
 export class Database {
+
     #database = {}
+
+    constructor() {
+        fs.readFile(databasePath, 'utf8').then(data => {
+            this.#database = JSON.parse(data)
+        })
+        .catch(() => {
+            this.#persist()
+        })
+    }
+
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
 
     select(table) {
         const data = this.#database[table] ?? []
@@ -15,5 +34,7 @@ export class Database {
         } else {
             this.#database[table] = [data]
         }
+
+        this.#persist()
     }
 }
